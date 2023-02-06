a589513d16c182e461a526c4877db04ea8c174a9
Lizander Camilo del Sol <lizander.kmilo@gmail.com>
2023-02-06 13:26:08 -0300

    100% responsive pero sin version movil

--- src/pages/home/component/BannerDev/BigBanner.jsx
+++ src/pages/home/component/BannerDev/BigBanner.jsx
@@ -1,28 +1,18 @@
 import React from 'react'
+import { Container } from 'react-bootstrap'
 import imgBanner from '../../../../assets/devSafioImageBanner.svg'
+import NewContrat_article from './ui_NewContrat'
 
 const BigBanner = () => {
     return <>
-        <div className='container-big-banner'>
-            <div className='big-banner-left'>
-                <p className='big--text--title'
-                    style={{ color: '#ffff' }}>
-                    ¿Buscas talento TI?
-                </p>
-                <div className='medium--text--title article-tittle'
-                    style={{ color: '#ffff' }}>
-                    <span>Contrata</span> <span style={{ color: 'rgba(0, 143, 247, 1)' }}>Talento TI </span><span>rápdo,</span><br />
-                    inteligente y eficiente
-                </div>
-                <p className='info--text--normal'
-                style={{ color: '#ffff' }}>
-                    Te encontramos profesionales en 5 días y los acompañamos por 3 meses con tutores senior para potenciar y acelerar sus habilidades<br/> técnicas
-                </p>
-                <input className='btn-dev info--text--normal btn-white btn-contact-now' type={'button'} value={'Quiero contratar'}></input>
-            </div>
-
+        <div className='container-big-banner' >
+            <Container className='big-banner-left'>
+                <NewContrat_article />
+            </Container>
             <div className='big-banner-right'>
-                <img src={imgBanner}></img>
+                <div className='container-img-banner'>
+                    <img src={imgBanner}></img>
+                </div>
             </div>
 
         </div>
--- src/pages/home/component/BannerDev/index.jsx
+++ src/pages/home/component/BannerDev/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Container } from 'react-bootstrap'
 import BigBanner from './BigBanner'
 import Navbartop from './Navbartop'
 
