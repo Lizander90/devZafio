2776d9f4bca21f120dcd04366728e9814a0ccd60
Lizander Camilo del Sol <lizander.kmilo@gmail.com>
2023-02-10 14:34:00 -0300

    banner responisvo 100% con section1-- falta refactorizar scss-- menu toogleable

--- src/pages/home/component/BannerDev/Navbartop.jsx
+++ src/pages/home/component/BannerDev/Navbartop.jsx
@@ -1,25 +1,60 @@
-import React from 'react'
+import React, { useMemo, useState } from 'react'
 import './style/Navbartop.scss'
 import logo from '../../../../assets/devSafioLogo.svg'
 
+// instale react-bootstrap-icons facilita el trabajo con iconos 
+import { DistributeVertical, XLg } from 'react-bootstrap-icons';
+
 const Navbartop = () => {
-    return (
+    const SCREEN_BREAK_POINT = 760;
+
+    const getDimensionWindows = () => {
+        const { innerWidth: width, innerHeight: height } = window;
+        return width;
+    }
+    const [movilMode, setMovilMode] = useState(getDimensionWindows() <= SCREEN_BREAK_POINT);
+
+    //EL COMPONENETE SOLO SE RENDERIZA CUANDO CAMBIE DE VALOR movilMode
+    window.addEventListener('resize', () => { 
+        console.log(getDimensionWindows())
+        setMovilMode(getDimensionWindows() <= SCREEN_BREAK_POINT) })
+
+
+
+
+
+
+    return (<>
         <div className='navbar-top'>
             <div className='navbar-part-left'><img src={logo}></img></div>
-
             <div className='navbar-part-right'>
-                <div className='nav-ul-list info--text--normal'>
-                    <a href='#'>Inicia sesión</a>
-                    <a href='#'>Administra la página</a>
-                </div>
+                {/* //modo desktop */}
+                {!movilMode &&
+                    <>
+                        <div className='nav-ul-list info--text--normal'>
+                            <a href='#'>Inicia sesión</a>
+                            <a href='#'>Administra la página</a>
+                        </div>
+                        <div className='container-button'>
+                            <input type={'button'} className='btn-dev info--text--normal btn-transparent ' value={'Registrate'} />
+                            <input type={'button'} className='btn-dev info--text--normal btn-white' value={'Contáctanos'} />
+                        </div>
+                    </>
+                }
+                {movilMode && <>
+                    <DistributeVertical className='tooggle--icon--menu'/>
+                    {/* , XLg  */}
+                </>
 
-                <div className='container-button'>
-                    <input type={'button'} className='btn-dev info--text--normal btn-transparent ' value={'Registrate'} />
-                    <input type={'button'} className='btn-dev info--text--normal btn-white' value={'Contáctanos'} />
-                </div>
+                }
             </div>
 
         </div>
+
+
+
+        {/* <a href='#'>{movilMode}</a> */}
+    </>
     )
 }
 
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import BannerDev from './component/BannerDev'
-
+import Section_whoWork from './component/Sections_whoWok/index'
 const Home_ui = () => {
     return <>
         <BannerDev />
-        {/* <Section_Who_Work />
-        <Section_Without_Junior />
+        {/* <div style={{backgroundColor:'grey'}}> Baner de prueba </div> */}
+        <Section_whoWork />
+        {/* <Section_Without_Junior />
         <Section_agenda />
         <Footer_Dev /> */}
     </>
 }
+
 
 export default Home_ui
